5a3a272cc8459a02fc34d0e0e307cab1d60f04bb
Ivan Petkov <ipetkow90@gmail.com>
2021-01-03 15:11:30 +0200

    lab tasks 01-06 solved

---
+++ 02.DATA TYPES AND VARIABLES/src/lab_tasks/Lab03_ExactSumOfRealNumbers.java
@@ -0,0 +1,19 @@
+package lab_tasks;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Lab03_ExactSumOfRealNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber = Integer.parseInt(scanner.nextLine());
+        BigDecimal sum = BigDecimal.ZERO;
+
+        for (int i = 0; i < inputNumber; i++) {
+            BigDecimal currentNumber = new BigDecimal(scanner.nextLine());
+            sum = sum.add(currentNumber);
+        }
+        System.out.println(sum);
+    }
+}
---
+++ 02.DATA TYPES AND VARIABLES/src/lab_tasks/Lab05_ConcatNames.java
@@ -0,0 +1,14 @@
+package lab_tasks;
+
+import java.util.Scanner;
+
+public class Lab05_ConcatNames {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String firstName = scanner.nextLine();
+        String secondName = scanner.nextLine();
+        String delimiter = scanner.nextLine();
+        System.out.printf("%s%s%s%n", firstName, delimiter, secondName);
+    }
+}
