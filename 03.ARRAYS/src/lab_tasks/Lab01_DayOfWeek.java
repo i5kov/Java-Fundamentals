2f4604a07924b2df66bcad927b850986a4230e18
Ivan Petkov <ipetkow90@gmail.com>
2021-01-04 21:59:08 +0200

    lab tasks 01-04 solved

---
+++ 03.ARRAYS/src/lab_tasks/Lab01_DayOfWeek.java
@@ -0,0 +1,19 @@
+package lab_tasks;
+
+import java.util.Scanner;
+
+public class Lab01_DayOfWeek {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int dayAsNumber = Integer.parseInt(scanner.nextLine());
+
+        String[] daysOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        if (dayAsNumber <= 0 || dayAsNumber > 7) {
+            System.out.println("Invalid day!");
+        } else {
+            System.out.println(daysOfWeek[dayAsNumber - 1]);
+        }
+    }
+}
