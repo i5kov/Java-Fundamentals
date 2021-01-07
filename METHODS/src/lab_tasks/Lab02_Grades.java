1a42215aba55184d9fdddd5574cb6f10afaffb2a
Ivan Petkov <ipetkow90@gmail.com>
2021-01-07 21:27:09 +0200

    lab tasks 01-05 solved

---
+++ METHODS/src/lab_tasks/Lab02_Grades.java
@@ -0,0 +1,28 @@
+package lab_tasks;
+
+import java.util.Scanner;
+
+public class Lab02_Grades {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double grade = Double.parseDouble(scanner.nextLine());
+        printGradeInWords(grade);
+    }
+
+    private static void printGradeInWords(double grade) {
+        String gradeAsWord = "";
+        if (grade >= 2.00 && grade <= 2.99) {
+            gradeAsWord = "Fail";
+        } else if (grade <= 3.49) {
+            gradeAsWord = "Poor";
+        } else if (grade <= 4.49) {
+            gradeAsWord = "Good";
+        } else if (grade <= 5.49) {
+            gradeAsWord = "Very good";
+        } else if (grade <= 6.00) {
+            gradeAsWord = "Excellent";
+        }
+        System.out.println(gradeAsWord);
+    }
+}
