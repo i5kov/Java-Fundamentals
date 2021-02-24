3f3e43efccbc0d263e355956480b19256a8aedb1
Ivan Petkov <ipetkow90@gmail.com>
2021-02-24 23:41:24 +0200

    midexam 06: solving task01: Black Flag

---
+++ 22.Exam Preparation/src/midexam_06/Task01_BlackFlag.java
@@ -0,0 +1,33 @@
+package midexam_06;
+
+import java.util.Scanner;
+
+public class Task01_BlackFlag {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int days = Integer.parseInt(scanner.nextLine());
+        int dailyPlunder = Integer.parseInt(scanner.nextLine());
+        double expectedPlunder = Double.parseDouble(scanner.nextLine());
+
+        double gatheredPlunder = 0;
+
+
+        for (int i = 1; i <= days; i++) {
+            gatheredPlunder += dailyPlunder;
+            if (i % 3 == 0) {
+                double additionalPlunder = dailyPlunder * 0.5;
+                gatheredPlunder += additionalPlunder;
+            }
+            if (i % 5 == 0) {
+                gatheredPlunder -= gatheredPlunder * 0.3;
+            }
+        }
+
+        if (gatheredPlunder >= expectedPlunder) {
+            System.out.printf("Ahoy! %.2f plunder gained.", gatheredPlunder);
+        } else {
+            System.out.printf("Collected only %.2f%% of the plunder.", (expectedPlunder - gatheredPlunder));
+        }
+    }
+}
