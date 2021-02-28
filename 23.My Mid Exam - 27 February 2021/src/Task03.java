6f09c5711876ca9c2cdbbb3504f35b9a1df17c59
Ivan Petkov <ipetkow90@gmail.com>
2021-02-28 19:20:02 +0200

    adding methods in the code

--- 23.My Mid Exam - 27 February 2021/src/Task03.java
+++ 23.My Mid Exam - 27 February 2021/src/Task03.java
@@ -22,7 +22,7 @@ public class Task03 {
                     if (!cards.contains(cardName)) {
                         System.out.println("Card not found.");
                     } else {
-                        myDeck.add(cardName);
+                        addCardInDeck(cardName, myDeck);
                     }
                     break;
                 case "Insert":
@@ -30,29 +30,49 @@ public class Task03 {
                     if ((index < 0 || index >= myDeck.size()) || !cards.contains(cardName)) {
                         System.out.println("Error!");
                     } else {
-                        myDeck.add(index, cardName);
+                        addCardInDeckAtSpecificPosition(cardName, index, myDeck);
                     }
                     break;
                 case "Remove":
                     if (!myDeck.contains(cardName)) {
                         System.out.println("Card not found.");
                     } else {
-                        myDeck.remove(cardName);
+                        removeCardFromDeck(cardName, myDeck);
                     }
                     break;
                 case "Swap":
                     String card2Name = tokens[2];
-                    int card1Position = myDeck.indexOf(cardName);
-                    int card2Position = myDeck.indexOf(card2Name);
-                    myDeck.set(card1Position, card2Name);
-                    myDeck.set(card2Position, cardName);
+                    swapTwoCardsInDeck(cardName, card2Name, myDeck);
                     break;
                 case "Shuffle":
-                    Collections.reverse(myDeck);
+                    shuffleCardsInDeck(myDeck);
                     break;
             }
             input = scanner.nextLine();
         }
         System.out.println(String.join(" ", myDeck));
+    }
+
+    private static void addCardInDeck(String cardName, List<String> deck) {
+        deck.add(cardName);
+    }
+
+    private static void addCardInDeckAtSpecificPosition(String cardName, int positionOfCard, List<String> deck) {
+        deck.add(positionOfCard, cardName);
+    }
+
+    private static void removeCardFromDeck(String cardName, List<String> deck) {
+        deck.remove(cardName);
+    }
+
+    private static void swapTwoCardsInDeck(String firstCardName, String secondCardName, List<String> deck) {
+        int card1Position = deck.indexOf(firstCardName);
+        int card2Position = deck.indexOf(secondCardName);
+        deck.set(card1Position, secondCardName);
+        deck.set(card2Position, firstCardName);
+    }
+
+    private static void shuffleCardsInDeck(List<String> deck) {
+        Collections.reverse(deck);
     }
 }
